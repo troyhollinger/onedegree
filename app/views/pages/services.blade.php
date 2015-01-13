816d50b661455a2380cca208cfd29823ceca87e4
Austen Payan <austenpayan@gmail.com>
2015-01-12 16:59:54 -0800

    Update services page styling

--- app/views/pages/services.blade.php
+++ app/views/pages/services.blade.php
@@ -15,8 +15,8 @@ Services
 
 		<div class="service-module">
 			<p class="section-title">Financial Planning</p>
-			<p class="text">We take a comprehensive approach to managing your wealth. By incorporating the six-step Financial Planning process recognized by the CFP® Board, your initial plan is developed over the course of several meetings. We also coordinate with your other professionals, such as estate planning attorneys, CPAs, and insurance specialists as needed. If you don’t have this team in place, we can provide recommendations.</p><br>
-			<p class="text">Financial plans are monitored and updated on a regular basis to ensure that as your life changes, your plan stays current. For affluent families and complex issues, we offer advanced planning to help address estate tax issues, charitable giving, business planning and legacy topics that often require special attention. Wealth Management Video Explainer</p><br>
+			<p class="text">We take a comprehensive approach to managing your wealth. By incorporating the <span class="green-text">six-step Financial Planning process</span> recognized by the CFP® Board, your initial plan is developed over the course of several meetings. We also coordinate with your other professionals, such as estate planning attorneys, CPAs, and insurance specialists as needed. If you don’t have this team in place, we can provide recommendations.</p><br>
+			<p class="text">Financial plans are monitored and updated on a regular basis to ensure that as your life changes, your plan stays current. For affluent families and complex issues, we offer advanced planning to help address estate tax issues, charitable giving, business planning and legacy topics that often require special attention. <span class="green-text">Wealth Management Video Explainer</span></p><br>
 		</div>
 
 		<div class="service-module">
@@ -45,7 +45,7 @@ Services
 				expanded scope of information on this lovely topic. 
 				Perhaps we can even aim to make it as long as the other 
 				two topics as viewed to the left. And it would be cool to 
-				do Video Explanations of all three Services listed.
+				do <span class="green-text">Video Explanations of all three Services</span> listed.
 			</p>
 
 		</div>
@@ -77,6 +77,76 @@ Services
 		<div class="info-module">
 
 			<p class="section-title">Frequently Asked Questions</p>
+
+			<div class="faq-column">
+				<p class="text">
+					<span class="bold">What types of clients do you work with?</span><br>
+					Our clients have many different backgrounds but there 
+					are a number of common traits among them. They 
+					have the ability to become or already are financially 
+					independent. They prefer to free up their time and use 
+					our experience, expertise and tools to delegate some 
+					of the responsibility of managing their wealth. Though 
+					we do not follow strict guidelines for client 
+					qualification, those who receive the most value 
+					typically meet some or all of the following:
+				</p>
+				<br>
+				<p class="text">
+					Income in excess of $150,000/year
+					Investable assets of $350,000 or greater
+					A net worth of $1M or greater
+				</p>
+				<br>
+				<br>
+				<p class="text">
+					<span class="bold">Who is Securities America, Inc.?</span><br>
+					Securities America is our broker/dealer. Think of them 
+					as behind-the-scenes support to us. They provide 
+					technology, compliance and operational services to 
+					help us provide comprehensive and professional 
+					services. They were established in 1984, are 
+					headquartered in Omaha, NE, and serve over 1,700 
+					financial advisors across the United States.
+				</p>
+			</div>
+			
+			<div class="faq-column">
+
+				<p class="text">
+
+					<span class="bold">What is a Certified Financial Planner?</span><br>
+					Most people think that all financial planners are 
+					"certified," but this isn’t true. Only those who have 
+					fulfilled the certification and renewal requirements of 
+					the CFP Board can display the CFP® certification 
+					marks. When selecting a financial planner, you need 
+					to feel confident that the person you choose to help 
+					you plan for your future is competent and ethical. The 
+					CFP® certification provides that sense of security by 
+					allowing only those who meet strict education, 
+					examination, experience and ethics requirements the 
+					right to use the CFP® certification marks.
+
+				</p>
+				<br>
+				<br>
+				<p class="text">
+
+					<span class="bold">How are you paid? </span><br>
+					We are fee-based, meaning most of our revenue 
+					comes from the fees paid for either financial planning 
+					and/or investment management services. Some fees 
+					are paid directly while others are paid from your 
+					investment accounts. On occasion, we may use a 
+					commissionable product if we feel it is in the best 
+					interest of the client. All fees are clearly disclosed. For 
+					more information or to request a fee schedule, please 
+					contact our office.
+
+				</p>
+
+			</div>
 
 		</div>
 
