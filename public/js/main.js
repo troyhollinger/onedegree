8cc62ed5570be316b578b2caee8728060f992b8c
Austen Payan <austenpayan@gmail.com>
2015-04-06 12:29:48 -0700

    attempt IE logo fix

--- public/js/main.js
+++ public/js/main.js
@@ -254,6 +254,32 @@ function isTouchDevice() {
 }
 
 
+function isIE() {
+    var ua = window.navigator.userAgent;
+
+    var msie = ua.indexOf('MSIE ');
+    if (msie > 0) {
+        // IE 10 or older => return version number
+        return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
+    }
+
+    var trident = ua.indexOf('Trident/');
+    if (trident > 0) {
+        // IE 11 => return version number
+        var rv = ua.indexOf('rv:');
+        return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
+    }
+
+    var edge = ua.indexOf('Edge/');
+    if (edge > 0) {
+       // IE 12 => return version number
+       return parseInt(ua.substring(edge + 5, ua.indexOf('.', edge)), 10);
+    }
+
+    // other browser
+    return false;
+}
+
 function mobile() {
 	
 	if ($(window) < 1100) {
@@ -408,6 +434,7 @@ var Mast = {
 
 	init : function() {
 
+		Mast.internetExplorerFix();
 		
 	},
 
@@ -448,6 +475,16 @@ var Mast = {
 		}
 
 		Mast.loop = requestAnimationFrame(Mast.coloring);
+
+	},
+
+	internetExplorerFix : function() {
+
+		if (isIE !== false) {
+
+			Mast.logo.height(50);
+
+		}
 
 	}
 
--- public/production/app.js
+++ public/production/app.js
@@ -1687,6 +1687,32 @@ function isTouchDevice() {
 }
 
 
+function isIE() {
+    var ua = window.navigator.userAgent;
+
+    var msie = ua.indexOf('MSIE ');
+    if (msie > 0) {
+        // IE 10 or older => return version number
+        return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
+    }
+
+    var trident = ua.indexOf('Trident/');
+    if (trident > 0) {
+        // IE 11 => return version number
+        var rv = ua.indexOf('rv:');
+        return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
+    }
+
+    var edge = ua.indexOf('Edge/');
+    if (edge > 0) {
+       // IE 12 => return version number
+       return parseInt(ua.substring(edge + 5, ua.indexOf('.', edge)), 10);
+    }
+
+    // other browser
+    return false;
+}
+
 function mobile() {
 	
 	if ($(window) < 1100) {
@@ -1841,6 +1867,7 @@ var Mast = {
 
 	init : function() {
 
+		Mast.internetExplorerFix();
 		
 	},
 
@@ -1881,6 +1908,16 @@ var Mast = {
 		}
 
 		Mast.loop = requestAnimationFrame(Mast.coloring);
+
+	},
+
+	internetExplorerFix : function() {
+
+		if (isIE !== false) {
+
+			Mast.logo.height(50);
+
+		}
 
 	}
 
