bd1d2540193a7c42e21a4ffae1d54dfb1c8199fa
Austen Payan <austenpayan@gmail.com>
2016-06-17 09:04:00 -0700

    revert testing environment

--- app/views/pages/clientLogin.blade.php
+++ app/views/pages/clientLogin.blade.php
@@ -8,15 +8,10 @@ One Degree Advisors - San Diego based financial planning and investment services
 client login
 @stop
 
-
-
 @section('content')
 
 <div class="container content indicator-parent home-services-module-wrapper" data-indicator="Cleint Login" id="client-login">
-
     <div class="home-service-modules-container">
-    
-
         <div class="service-module">
             <button class="login-btn one-view"  onclick="window.open('https://wealth.emaplan.com/ema/Default.aspx?ema/securitiesamerica/onedegree', '_blank');">One View</button><br><br>
             <p class="text login-text">This provides information on your
@@ -26,99 +21,49 @@ client login
             </p><br>
             <p class="text login-text">
                 <span class="bold">Helpful hints on navigating website:</span>
-From the <span class="bold">Home</span> tab you can access the
-Tour Guide video in the bottom right tile.
-This will give a brief overview of website
-and access abilities.
+                From the <span class="bold">Home</span> tab you can access the Tour Guide video in the bottom right tile. This will give a brief overview of website and access abilities.
             </p><br>
             <p class="text login-text">
-                From the <span class="bold">Organizer</span> tab accounts can be
-connected to the plan.
+                From the <span class="bold">Organizer</span> tab accounts can be connected to the plan.
             </p><br>
             <p class="text login-text light">
-                Please contact your advisor when you
-link an account. Your accounts under our
-management will be linked.
+                Please contact your advisor when you link an account. Your accounts under our management will be linked.
             </p><br>
             <p class="text login-text">
                 <span class="bold">
                     Is this secure? :
                 </span>
             </p>
-            
             <div class="iframe-container"><iframe width="560" height="315" src="https://www.youtube.com/embed/vruAo__5hMU" frameborder="0" allowfullscreen></iframe></div>
-
         </div>
-
         <div class="service-module">
-            <button class="login-btn performance-reports"  onclick="window.open('https://advisor.envestnet.com/secure/login/envest.jsp', '_blank');">Performance
-Reports</button><br><br>
-            <p class="text login-text">This illustrates Quarterly and Daily
-account performance and asset
-allocation.</p><br>
-<p class="text login-text">
-    <span class="bold">Helpful hints on navigating website:</span>
-The initial page, Market Summary,
-shows recent market related
-information.
-</p><br>
-<p class="text login-text">
-    The Investment Reports section is
-where you will find many different
-reports.
-</p><br><br>
-        
+            <button class="login-btn performance-reports"  onclick="window.open('https://advisor.envestnet.com/secure/login/envest.jsp', '_blank');">Performance Reports</button><br><br>
+            <p class="text login-text">This illustrates Quarterly and Daily account performance and asset allocation.</p><br>
+            <p class="text login-text"><span class="bold">Helpful hints on navigating website:</span> The initial page, Market Summary, shows recent market related information.</p>
+            <br>
+            <p class="text login-text">The Investment Reports section is where you will find many different reports.</p>
+            <br><br>
             <p class="text login-text">
                 <span class="bold">Forgot log-in information?</span><br>
-Email <a href="mailto:admin@onedegreeadvisors.com?Subject=Forgot%20login" target="_top">admin@onedegreeadvisors.com</a>
-Or Call 619 282-3288    
-            </p>
-                
-            
-
+                Email <a href="mailto:admin@onedegreeadvisors.com?Subject=Forgot%20login" target="_top">admin@onedegreeadvisors.com</a> Or Call 619 282-3288    
+            </p>                
         </div>
-
         <div class="service-module">
-
             <button class="login-btn td-ameritrade"  onclick="window.open('https://www.advisorclient.com/advisorclient/p/gridLoin', '_blank');">TD Ameritrade</button><br><br>
             <p class="text text-login">
-                This provides access to formal account
-statements, trade confirmations and tax
-forms for accounts held at TD
-Ameritrade.
+                This provides access to formal account statements, trade confirmations and tax forms for accounts held at TD Ameritrade.
             </p><br>
-            <p class="text text-login">
-                <span class="bold">
-                    Helpful hints on navigating website:
-                </span>
-From the top of any site page, you can
-access a <span class="bold">site map</span> for an overview of
-how the site is organized.
-            </p><br>
-            <p class="text text-login">
-                In <span class="bold">Accounts</span>, you'll find the <span class="bold">Account
-Center</span> which displays account
-information for a single account,
-including balances, positions, and
-history.
-            </p>
-
-          
-
+            <p class="text text-login"><span class="bold">Helpful hints on navigating website:</span> From the top of any site page, you can access a <span class="bold">site map</span> for an overview of how the site is organized.</p>
+            <br>
+            <p class="text text-login">In <span class="bold">Accounts</span>, you'll find the <span class="bold">Account Center</span> which displays account information for a single account, including balances, positions, and history.</p>    
         </div>
-
     </div>
-
 </div> 
-
 <div class="banner footer-banner">
-
     <div class="container">
         <p class="banner-quote">  Plans Fails for Lack of Counsel,<br> <span class="bold">But with Many Advisors they Succeed.</span></p>
         <p class="banner-quote-source">PROVERBS</p>
-
     </div>
-
 </div>
 
 @stop
